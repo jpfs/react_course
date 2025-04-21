f2125ae53408e696b5559e76a709c5210203a6c9
Joao Santos <jp.f.s@hotmail.com>
2025-04-21 12:52:15 +0100

    commit

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -4,6 +4,10 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  function handleSelect() {
+    console.log("Hello World!");
+  }
+
   return (
     <div>
       <Header />
@@ -38,10 +42,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JXS</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JXS</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
           </menu>
         </section>
       </main>
