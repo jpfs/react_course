3b13bad5dd3ec31b736cd7120d855b27ad1e985c
Joao Santos <jp.f.s@hotmail.com>
2025-04-21 11:56:31 +0100

    commit

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,48 +1,12 @@
-import reactImg from "./assets/react-core-concepts.png";
 import componentsImg from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title}></img>
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-  {
-    /* 
-function CoreConcept({}) {
-  return (
-    <li>
-      <img src={image} alt={title}></img>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-  */
-  }
 }
 
 function App() {
---
+++ 01-starting-project/src/components/CoreConcept.jsx
@@ -0,0 +1,19 @@
+export default function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title}></img>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+  /* 
+  function CoreConcept({}) {
+    return (
+      <li>
+        <img src={image} alt={title}></img>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+    );
+    */
+}
