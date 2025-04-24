06e74a7d06c741cab21fc8806e5f4b5ab51d31e9
Joao Santos <jp.f.s@hotmail.com>
2025-04-24 09:41:09 +0100

    commit

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -5,9 +5,10 @@ import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 import componentsImg from "./assets/components.png";
+import { EXAMPLES } from "./data.js";
 
 function App() {
-  cons[(selectedTopic, setSelectedTopic)] = useState("please click a button");
+  cons[(selectedTopic, setSelectedTopic)] = useState("components");
 
   function handleSelect(selectButton) {
     // selectButton => "components, jxs, props, state"
@@ -57,7 +58,13 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
