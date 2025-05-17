f00c51266f588fda7d3817a917ad4826889da04a
Joao Santos <jp.f.s@hotmail.com>
2025-05-17 09:36:18 +0100

    commit

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,22 @@
-
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span class="player-name">Player 1</span>
+            <span class="player-symbol">X</span>
+          </li>
+          <li>
+            <span class="player-name">Player 2</span>
+            <span class="player-symbol">O</span>
+          </li>
+        </ol>
+        Game board
+      </div>
+      log
+    </main>
+  );
 }
 
-export default App
+export default App;
