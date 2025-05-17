92c3212766f327f1bcd90ad3d838ec38425e659a
Joao Santos <jp.f.s@hotmail.com>
2025-05-17 10:00:45 +0100

    commit

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,16 +1,12 @@
+import Player from "./components/Player.jsx";
+
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span class="player-name">Player 1</span>
-            <span class="player-symbol">X</span>
-          </li>
-          <li>
-            <span class="player-name">Player 2</span>
-            <span class="player-symbol">O</span>
-          </li>
+          <Player name="Player 1" symbol="x" />
+          <Player name="Player 2" symbol="o" />
         </ol>
         Game board
       </div>
